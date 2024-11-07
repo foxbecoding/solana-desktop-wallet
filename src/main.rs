8e567df63cd9f850ea30f9cb80288b5b27f9700e
foxbecoding <foxbecoding@gmail.com>
2024-11-07 09:05:48 -0600

    Add App module and update main entry point
    Created a new `App` module with a `start` function to encapsulate application startup logic. Updated `main.rs` to utilize `app::App::start()` for improved code organization. Modified `.env_example` to include a new environment variable `APP_ASSETS_PATH`.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod app;
+
 use slint::{PlatformError, include_modules as include_slint_modules};
 
 include_slint_modules!();
 fn main() -> Result<(), PlatformError> {
-    let app = App::new()?;
-    app.run()?;
+    app::App::start()?;
     Ok(())
 }
