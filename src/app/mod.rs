23f2eb4fdac902a5eb83ee4f67157efd926c6427
foxbecoding <foxbecoding@gmail.com>
2024-11-09 19:31:19 -0600

    Remove testing code from app initialization
    Commented out the code that sets up a testing wallet and its related data structures. This clean-up ensures that the main application runs without any test data interfering with its normal operation.

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -31,27 +31,27 @@ impl App {
         let app = crate::App::new()?;
 
         //for testing
-        let pubkey = Pubkey::new_unique();
-
-        let wallet =
-            SlintWallet {
-                id: 1,
-                name: SharedString::from("Main Account".to_string()),
-                seed: SharedString::from("Some Seed Phrase".to_string()),
-                public_key: SharedString::from(pubkey.to_string()),
-                public_key_display: pubkey_display_generator(pubkey.to_string()),
-                is_passphrase_protected: false,
-            };
-
-        let wallets = vec![wallet.clone()];
-
-
-        // used to set wallets vector
-        let rc_wallets: Rc<VecModel<SlintWallet>> = Rc::new(VecModel::from(wallets));
-        let model_rc_wallets = ModelRc::from(rc_wallets.clone());
-        crate::WalletManager::get(&app).set_wallets(model_rc_wallets);
-
-        crate::WalletManager::get(&app).set_selected_wallet(wallet);
+        // let pubkey = Pubkey::new_unique();
+        //
+        // let wallet =
+        //     SlintWallet {
+        //         id: 1,
+        //         name: SharedString::from("Main Account".to_string()),
+        //         seed: SharedString::from("Some Seed Phrase".to_string()),
+        //         public_key: SharedString::from(pubkey.to_string()),
+        //         public_key_display: pubkey_display_generator(pubkey.to_string()),
+        //         is_passphrase_protected: false,
+        //     };
+        //
+        // let wallets = vec![wallet.clone()];
+        //
+        //
+        // // used to set wallets vector
+        // let rc_wallets: Rc<VecModel<SlintWallet>> = Rc::new(VecModel::from(wallets));
+        // let model_rc_wallets = ModelRc::from(rc_wallets.clone());
+        // crate::WalletManager::get(&app).set_wallets(model_rc_wallets);
+        //
+        // crate::WalletManager::get(&app).set_selected_wallet(wallet);
 
         app.run()?;
         Ok(())
