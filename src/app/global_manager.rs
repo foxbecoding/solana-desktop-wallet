eec5a32529d52e5d55fa5ffd6f120b6906168276
foxbecoding <foxbecoding@gmail.com>
2024-11-13 13:26:14 -0600

    Add CallbackManager import and empty implementation
    This change imports the CallbackManager and includes an empty implementation for it within the GlobalManager structure. This lays the groundwork for integrating callback handling into GlobalManager in future commits.

--- src/app/global_manager.rs
+++ src/app/global_manager.rs
@@ -1,6 +1,9 @@
+use crate::app::callback_manager::CallbackManager;
 use crate::database::account::Account;
 
 pub struct GlobalManager<'a>  {
     app_instance: &'a crate::App,
     accounts: Vec<Account>
 }
+
+impl<'a> CallbackManager<'a> {}
