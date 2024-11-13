0c9669aacc57244b151083cacf95bcae73f95646
foxbecoding <foxbecoding@gmail.com>
2024-11-13 13:47:39 -0600

    Add slint account builder to global manager
    Refactor `slint_account_builder` function to return `SlintAccount` and utilize `SharedString` for `name`, `seed`, and `pubkey` fields. This enhances type safety and improves integration with the Slint-generated code.

--- src/app/global_manager.rs
+++ src/app/global_manager.rs
@@ -1,6 +1,7 @@
+use slint::SharedString;
 use crate::app::errors::AppError;
 use crate::database::account::Account;
-
+use crate::slint_generatedApp::Account as SlintAccount;
 pub struct GlobalManager<'a>  {
     app_instance: &'a crate::App,
     accounts: &'a Vec<Account>
@@ -25,6 +26,12 @@ impl<'a> GlobalManager<'a> {
     }
 }
 
-fn slint_account_builder(account: &Account)  {
-
+fn slint_account_builder(account: &Account) -> SlintAccount{
+    SlintAccount {
+        id: account.id.unwrap(),
+        name: SharedString::from(account.name.clone()),
+        seed: SharedString::from(account.seed.clone()),
+        pubkey: SharedString::from(account.pubkey.clone()),
+        pubkey_display: account.pubkey_display(),
+    }
 }
