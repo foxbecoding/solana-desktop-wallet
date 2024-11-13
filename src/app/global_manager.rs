05a3999057bfab03cb93519670a615dc900ada06
foxbecoding <foxbecoding@gmail.com>
2024-11-13 13:32:16 -0600

    Refactor GlobalManager to accept accounts by reference
    Update the GlobalManager struct to hold a reference to accounts instead of owning a vector. This change also modifies the constructor to accept and set the accounts reference, improving memory management.

--- src/app/global_manager.rs
+++ src/app/global_manager.rs
@@ -2,10 +2,11 @@ use crate::database::account::Account;
 
 pub struct GlobalManager<'a>  {
     app_instance: &'a crate::App,
-    accounts: Vec<Account>
+    accounts: &'a Vec<Account>
 }
 
 impl<'a> GlobalManager<'a> {
-    pub fn new(app_instance: &'a crate::App) {
+    pub fn new(app_instance: &'a crate::App, accounts: &'a Vec<Account>) -> Self {
+        GlobalManager { app_instance, accounts }
     }
 }
