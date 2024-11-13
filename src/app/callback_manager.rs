19bddd20c2e679375d50b635c7db8a83b8fa0ab2
foxbecoding <foxbecoding@gmail.com>
2024-11-13 11:16:44 -0600

    Add view_account_handler to CallbackManager
    This handler opens the Solana account URL in the default web browser. It provides feedback on whether the browser was successfully opened or not.

--- src/app/callback_manager.rs
+++ src/app/callback_manager.rs
@@ -1,9 +1,24 @@
+use slint::ComponentHandle;
+use solana_sdk::msg;
+
 struct CallbackManager<'a>  {
     app_instance: &'a crate::App
 }
 
 impl CallbackManager {
-    fn new(app_instance: &crate::App) -> Self {
+    pub fn new(app_instance: &crate::App) -> Self {
         CallbackManager { app_instance }
+    }
+
+    fn view_account_handler(&self) {
+        self.app_instance.global::<crate::AccountManager>().on_view_account(|pubkey| {
+            let url = format!("https://solscan.io/account/{}", pubkey);
+
+            if webbrowser::open(url.as_str()).is_ok() {
+                msg!("Opened '{}' in your default web browser.", pubkey);
+            } else {
+                msg!("Failed to open '{}'.", pubkey);
+            }
+        });
     }
 }
