add45fac64f78de6b99a1b882d8d848e907d88e4
foxbecoding <foxbecoding@gmail.com>
2024-11-12 22:14:27 -0600

    Refactor `run_app` and add `view_account_handler`
    Refactored the `run_app` method by extracting the account view handling logic into a new `view_account_handler` function. This enhances readability and modularity of the code by separating concerns. The new function encapsulates the logic for handling the side navigation account view and opening the URL in the default web browser.

--- src/app/app.rs
+++ src/app/app.rs
@@ -24,21 +24,21 @@ impl App {
     fn run_app(&self) -> Result<(), AppError> {
         let app = crate::App::new()?;
         self.set_app_globals(&app)?;
+        self.view_account_handler(&app);
+        app.run()?;
+        Ok(())
+    }
 
+    fn view_account_handler(&self, app: &crate::App) {
         app.global::<SideNavAccountGlobals>().on_view_account(|pubkey| {
             let url = format!("https://solscan.io/account/{}", pubkey);
 
-            //Todo find a better way to display error if it doesn't work
-            // Attempt to open the URL in the default web browser
             if webbrowser::open(url.as_str()).is_ok() {
                 msg!("Opened '{}' in your default web browser.", pubkey);
             } else {
                 msg!("Failed to open '{}'.", pubkey);
             }
         });
-
-        app.run()?;
-        Ok(())
     }
 
     fn set_app_globals(&self, app: &crate::App) -> Result<(), AppError> {
