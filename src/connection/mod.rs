051ddcc10c10d05f4260a22a8fb8d1fb53309a4c
foxbecoding <foxbecoding@gmail.com>
2024-11-15 19:14:24 -0600

    Add network URL configuration based on environment variables
    Refactor to retrieve Solana network URLs from environment variables and handle unknown networks with an error message. Update .env_example to include NETWORK and Solana-specific URL variables for mainnet, devnet, and testnet.

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -1,10 +1,28 @@
+use std::env;
 use solana_rpc_client::rpc_client::RpcClient;
 
 pub struct Connection {}
 
 impl Connection {
-    fn network() {
+    fn solana_url() -> String {
+        // Retrieve the NETWORK environment variable
+        let network = env::var("NETWORK").unwrap_or_else(|_| "devnet".to_string());
 
+        // Define the URLs
+        let solana_mainnet = env::var("SOLANA_MAINNET").unwrap_or_else(|_| "https://api.mainnet-beta.solana.com".to_string());
+        let solana_devnet = env::var("SOLANA_DEVNET").unwrap_or_else(|_| "https://api.devnet.solana.com".to_string());
+        let solana_testnet = env::var("SOLANA_TESTNET").unwrap_or_else(|_| "https://api.testnet.solana.com".to_string());
+
+        // Match the NETWORK variable and return the corresponding URL
+        match network.as_str() {
+            "mainnet" => solana_mainnet,
+            "devnet" => solana_devnet,
+            "testnet" => solana_testnet,
+            _ => {
+                eprintln!("Unknown network: {}", network);
+                std::process::exit(1);
+            }
+        }
     }
 
     pub fn connection(&self) -> RpcClient {}
