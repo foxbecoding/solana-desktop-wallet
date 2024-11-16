27d7296a734a4a3d4eeaaaea0f7821cc09fb1ea6
foxbecoding <foxbecoding@gmail.com>
2024-11-15 19:35:07 -0600

    Refactor network handling in `Connection` struct.
    Moved the network initialization into the `Connection::new` method and encapsulated network string conversion in a new `from_str` method for `ConnectionNetwork`. This refactoring simplifies the URL matching logic and removes redundant code.

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -7,48 +7,41 @@ pub enum ConnectionNetwork {
     TESTNET
 }
 
+impl ConnectionNetwork {
+    fn from_str(network: &str) -> Option<Self> {
+        match network.to_lowercase().as_str() {
+            "mainnet" => Some(ConnectionNetwork::MAINNET),
+            "devnet" => Some(ConnectionNetwork::DEVNET),
+            "testnet" => Some(ConnectionNetwork::TESTNET),
+            _ => None,
+        }
+    }
+}
+
 pub struct Connection {
     pub network: ConnectionNetwork,
 }
 
 impl Connection {
-    pub fn new(&self) -> Self {}
+    pub fn new(&self) -> Self {
+        let network_env = env::var("NETWORK").unwrap_or_else(|_| "devnet".to_string());
+        let network = ConnectionNetwork::from_str(&network_env).expect("invalid network");
+        Self { network }
+    }
 
-    fn solana_url() -> String {
-        // Retrieve the NETWORK environment variable
-        let network = env::var("NETWORK").unwrap_or_else(|_| "devnet".to_string());
-
+    fn solana_url(&self) -> String {
         // Define the URLs
         let solana_mainnet = env::var("SOLANA_MAINNET").unwrap_or_else(|_| "https://api.mainnet-beta.solana.com".to_string());
         let solana_devnet = env::var("SOLANA_DEVNET").unwrap_or_else(|_| "https://api.devnet.solana.com".to_string());
         let solana_testnet = env::var("SOLANA_TESTNET").unwrap_or_else(|_| "https://api.testnet.solana.com".to_string());
 
         // Match the NETWORK variable and return the corresponding URL
-        match network.as_str() {
-            "mainnet" => solana_mainnet,
-            "devnet" => solana_devnet,
-            "testnet" => solana_testnet,
-            _ => {
-                eprintln!("Unknown network: {}", network);
-                std::process::exit(1);
-            }
+        match self.network {
+            ConnectionNetwork::MAINNET => solana_mainnet,
+            ConnectionNetwork::DEVNET => solana_devnet,
+            ConnectionNetwork::TESTNET => solana_testnet,
         }
     }
 
     pub fn connection(&self) -> RpcClient {}
 }
-
-fn get_network() -> ConnectionNetwork {
-    let network = env::var("NETWORK").unwrap_or_else(|_| "devnet".to_string());
-
-    match network.as_str() {
-        "mainnet" => ConnectionNetwork::MAINNET,
-        "devnet" => ConnectionNetwork::DEVNET,
-        "testnet" => ConnectionNetwork::TESTNET,
-        _ => {
-            eprintln!("Unknown network: {}", network);
-            std::process::exit(1);
-        }
-    }
-}
-
