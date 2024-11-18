b89ef263c6cf9294ec34332aedc0c09cc96ce0b1
foxbecoding <foxbecoding@gmail.com>
2024-11-18 12:31:43 -0600

    Refactor database module imports and error handling
    Removed unnecessary imports of `DatabaseError` and `errors` module. Simplified the `database_connection` function to directly return the connection result.

--- src/database.rs
+++ src/database.rs
@@ -1,9 +1,6 @@
 pub mod account;
 pub mod errors;
-
 use rusqlite::{Connection, Result};
-use crate::database::errors::DatabaseError;
-
 
 pub fn database_connection() -> Result<Connection, errors::DatabaseError> {
     let conn = Connection::open("resources/database/database.db")?;
