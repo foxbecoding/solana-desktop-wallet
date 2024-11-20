46bc5203a628de5fcdbe3d0f6db889bbb234a54f
foxbecoding <foxbecoding@gmail.com>
2024-11-19 21:51:29 -0600

    Refactor app instance with Arc and Mutex
    This change wraps the app instance in Arc and Mutex to ensure thread safety during concurrent access. The run_app method creates an Arc-wrapped app instance, and run_managers clones this instance for global_manager and callback_manager to use.

--- src/app.rs
+++ src/app.rs
@@ -2,6 +2,7 @@ pub(crate) mod errors;
 pub(crate) mod callback_manager;
 pub(crate) mod global_manager;
 use slint::ComponentHandle;
+use std::sync::{Arc, Mutex};
 use crate::database::account::Account;
 
 #[derive(Debug)]
@@ -16,15 +17,19 @@ impl App {
     }
 
     fn run_app(&self) -> Result<(), errors::AppError> {
-        let app = crate::App::new()?;
-        self.run_managers(&app)?;
-        app.run()?;
+        let mut app = crate::App::new()?;
+
+        // Wrapping the mutable reference in Arc and Mutex
+        let arc_app_instance = Arc::new(Mutex::new(app));
+
+        self.run_managers(Arc::clone(&arc_app_instance))?;
+        arc_app_instance.lock().unwrap().run()?;
         Ok(())
     }
 
-    fn run_managers(&self, app: &crate::App) -> Result<(), errors::AppError> {
-        global_manager::GlobalManager::new(&app, &self.accounts).run()?;
-        callback_manager::CallbackManager::new(&app).run()?;
+    fn run_managers(&self, arc_app_instance: Arc<Mutex<crate::App>>) -> Result<(), errors::AppError> {
+        global_manager::GlobalManager::new(Arc::clone(&arc_app_instance), &self.accounts).run()?;
+        callback_manager::CallbackManager::new(Arc::clone(&arc_app_instance)).run()?;
         Ok(())
     }
 }
