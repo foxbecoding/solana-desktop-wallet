fe7e5f6b6f58092f1a8628808db175a565873bb3
foxbecoding <foxbecoding@gmail.com>
2024-11-20 11:01:42 -0600

    Refactor GlobalManager to hold reference to app_instance
    Changed the GlobalManager struct to hold a reference to app_instance instead of owning it. This makes the memory management clearer and prevents unnecessary copies, ensuring more efficient use of resources.

--- src/app/global_manager.rs
+++ src/app/global_manager.rs
@@ -5,12 +5,12 @@ use crate::database::account::Account;
 use crate::slint_generatedApp::Account as SlintAccount;
 
 pub struct GlobalManager<'a>  {
-    app_instance: crate::App,
+    app_instance: &'a crate::App,
     accounts: &'a Vec<Account>
 }
 
 impl<'a> GlobalManager<'a> {
-    pub fn new(app_instance: crate::App, accounts: &'a Vec<Account>) -> Self {
+    pub fn new(app_instance: &'a crate::App, accounts: &'a Vec<Account>) -> Self {
         GlobalManager { app_instance, accounts }
     }
 
