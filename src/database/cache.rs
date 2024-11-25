40b27d67386ee43064d73684c20c08144ea47298
foxbecoding <foxbecoding@gmail.com>
2024-11-25 12:40:18 -0600

    Add serialization for cache values
    Introduced the `CacheValue` struct with `Serialize` and `Deserialize` traits to facilitate serialization and deserialization of cache values. This will help in storing and retrieving structured data in a more efficient manner.

--- src/database/cache.rs
+++ src/database/cache.rs
@@ -1,4 +1,12 @@
 use rusqlite::{Connection};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct CacheValue {
+    value: String,
+}
+
 struct Cache {
     conn: Connection,
 }
+
