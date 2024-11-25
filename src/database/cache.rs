d77c23ecd0f702de86b1e8b571b7c28ec7497bb0
foxbecoding <foxbecoding@gmail.com>
2024-11-25 12:43:18 -0600

    Add basic constructor to Cache struct
    Introduced a new constructor function 'new' for the Cache struct to facilitate easier initialization. This commit also includes the necessary imports for database connections and error handling.

--- src/database/cache.rs
+++ src/database/cache.rs
@@ -1,5 +1,6 @@
 use rusqlite::{Connection};
 use serde::{Deserialize, Serialize};
+use crate::database::{database_connection, errors::DatabaseError};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct CacheValue {
@@ -10,3 +11,8 @@ struct Cache {
     conn: Connection,
 }
 
+impl Cache {
+    fn new() -> Self {
+
+    }
+}
