5ce617de1da59b20995ea42973e4cdf36c8cdfdb
foxbecoding <foxbecoding@gmail.com>
2024-12-03 09:09:35 -0600

    Implements init method

---
+++ src/initializer.rs
@@ -0,0 +1,18 @@
+use crate::app::errors::AppError;
+use crate::database::account::{get_accounts, Account as AccountModel};
+
+pub fn init() -> Result<(), AppError> {
+    set_backend_renderer();
+    let mut accounts = get_accounts()?;
+    accounts = set_accounts_balances(accounts.clone())?;
+    let has_accounts = !accounts.is_empty();
+
+    if !has_accounts {
+        AccountModel::new()?;
+        accounts = get_accounts()?;
+    }
+
+    let app = app::App { accounts };
+    start_app(app)?;
+    Ok(())
+}
