ce37c41a24886ecd460b9333c5a56e0d383b0370
foxbecoding <foxbecoding@gmail.com>
2024-12-04 08:19:03 -0600

    Implements function for future test tables creation

--- src/database.rs
+++ src/database.rs
@@ -14,6 +14,8 @@ pub fn database_connection() -> Result<Connection, DatabaseError> {
     Ok(conn)
 }
 
+fn create_test_tables(conn: &Connection) {}
+
 fn create_test_accounts_table(conn: &Connection) {
     conn.execute(
         "CREATE TABLE accounts (
