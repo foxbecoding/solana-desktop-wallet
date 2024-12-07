72668174ddef39c2e8f4400afbfcf4dd1df13969
foxbecoding <foxbecoding@gmail.com>
2024-12-07 13:14:40 -0600

    Refactor removes unused method

--- src/services/account_service.rs
+++ src/services/account_service.rs
@@ -2,10 +2,7 @@ use rusqlite::{params, Connection};
 use slint::SharedString;
 use std::sync::{Arc, Mutex};
 
-use crate::database::{
-    account::{get_accounts, Account},
-    errors::DatabaseError,
-};
+use crate::database::{account::Account, errors::DatabaseError};
 
 pub struct AccountService {
     conn: Arc<Mutex<Connection>>,
@@ -38,10 +35,6 @@ impl AccountService {
 
     pub fn get_all_accounts(&self) -> Result<Vec<Account>, DatabaseError> {
         get_accounts(&self.conn)
-    }
-
-    pub fn get_account_balance(&self, account: &Account) -> f64 {
-        account.balance_in_sol()
     }
 
     pub fn generate_pubkey_display(&self, account: &Account) -> SharedString {
